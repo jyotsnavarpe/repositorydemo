23904e9a65c2c49709bdc3a601dd121bc275da57
admin <admin@DESKTOP-NBC9014>
2022-05-29 19:29:00 +0530

    first commit...

---
+++ src/gitDemo1/GtiTestDemo.java
@@ -0,0 +1,21 @@
+package gitDemo1;
+
+public class GtiTestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "jyotsna";
+		
+		if (name == "jyotsna")
+		   {
+				System.out.println("welcome");
+		   }
+		else
+		  {
+			System.out.println("please wait outside");
+		  }
+
+	}
+
+}
